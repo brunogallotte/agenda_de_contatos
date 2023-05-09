cf1162e5a55248585440183d61e550de8a69e6be
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-05-09 10:40:45 -0300

    Initial git

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('formulario-agenda');
+const tabela = document.querySelector('table');
+const nomeContato = document.getElementById('nome-contato');
+const numeroContato = document.getElementById('numero-contato');
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    criaLinha();
+    atualizaAgenda();
+    nomeContato.value = '';
+    numeroContato.value = '';
+})
+
+function criaLinha() {
+    let linha = '<tr>';
+    linha += `<td>${nomeContato.value}</td>`;
+    linha += `<td>${numeroContato.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+}
+
+function atualizaAgenda() {
+    corpoAgenda = document.querySelector('tbody');
+    corpoAgenda.innerHTML = linhas;
+}
